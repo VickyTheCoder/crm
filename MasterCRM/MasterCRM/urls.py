1f8e210ab7cec606c4b1ac15346e9959cf23199f
Yogesh <yogeshwaransiva65@gmail.com>
2025-09-12 15:38:57 +0530

    new commit

--- MasterCRM/MasterCRM/urls.py
+++ MasterCRM/MasterCRM/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from UI import views
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.homepage,name='homepage'),
     path('ui',include('UI.urls'))
 ]
--- MasterCRM/UI/urls.py
+++ MasterCRM/UI/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.default, name='default'),
-    path('',views.homepage,name='homepage')
 ]
